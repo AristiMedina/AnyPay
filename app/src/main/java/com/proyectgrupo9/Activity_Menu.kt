9b449611d3da309765967fc969cbcb530b2883d8
AristiMedina <isabelaristimedina07@gmail.com>
2022-11-05 10:22:26 -0500

    Fragmentos completados

--- app/src/main/java/com/proyectgrupo9/Activity_Menu.kt
+++ app/src/main/java/com/proyectgrupo9/Activity_Menu.kt
@@ -6,12 +6,32 @@ import android.view.View
 
 class Activity_Menu : AppCompatActivity() {
 
+    val frag_Cuenta = Fragment_cuenta();
+    val frag_Pagos = Fragment_pagos();
+    val frag_Resumen = Fragment_resumen();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-    }
-    fun unClick(view: View) {
+        supportFragmentManager.beginTransaction().add(R.id.contenedorMenu, frag_Cuenta).commit()
 
     }
+
+    fun unClick(view: View) {
+        val trans = supportFragmentManager.beginTransaction();
+
+        if(view.id == R.id.FragCuenta){
+            trans.replace(R.id.contenedorMenu, frag_Cuenta)
+        }
+        if(view.id == R.id.FragPagos){
+            trans.replace(R.id.contenedorMenu, frag_Pagos)
+        }
+        if(view.id == R.id.FragResumen){
+            trans.replace(R.id.contenedorMenu, frag_Resumen)
+        }
+
+        trans.commit()
+    }
+
 }
--- app/src/main/java/com/proyectgrupo9/Fragment_resumen.kt
+++ app/src/main/java/com/proyectgrupo9/Fragment_resumen.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -12,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragment_cuenta.newInstance] factory method to
+ * Use the [Fragment_resumen.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment_cuenta : Fragment() {
+class Fragment_resumen : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,10 +29,12 @@ class Fragment_cuenta : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cuenta, container, false)
+        return inflater.inflate(R.layout.fragment_resumen, container, false)
     }
 
     companion object {
@@ -41,15 +44,16 @@ class Fragment_cuenta : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_cuenta.
+         * @return A new instance of fragment Fragment_resumen.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                Fragment_cuenta().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment_resumen().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }
